a9c5167cd92f13c971c135cef9a355fe03af73a9
Bhargav <Bhargav@gmail.com>
2024-07-10 15:56:45 +0530

    my-first-commit

---
+++ src/test/java/com/credeze/swagger/SwaggerConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credeze.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
